48ee4b27de2dca2c43f2eca325e7b29316bdde74
ezr1234 <enzo.z.rodrigues@gmail.com>
2022-04-12 10:35:21 -0300

    First commit

---
+++ transmissor/src/main.cpp
@@ -0,0 +1,104 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp-now-esp32-arduino-ide/
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <esp_now.h>
+#include <WiFi.h>
+
+// Mac do receptor = A4:CF:12:25:D9:8C
+uint8_t broadcastAddress[] = {0xA4, 0xCF, 0x12, 0x25, 0xD9, 0x8C};
+
+#define btn0 19
+#define btn1 23
+
+// Structure example to send data
+// Must match the receiver structure
+typedef struct struct_message
+{
+  int btn;
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+
+esp_now_peer_info_t peerInfo;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
+{
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+void setup()
+{
+
+  pinMode(btn0, INPUT_PULLUP);
+  pinMode(btn1, INPUT_PULLUP);
+  // Init Serial Monitor
+  Serial.begin(115200);
+
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK)
+  {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  // Add peer
+  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+  {
+    Serial.println("Failed to add peer");
+    return;
+  }
+}
+
+void sendBtn(int btn)
+{
+  // Set values to send
+  myData.btn = btn;
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+
+  if (result == ESP_OK)
+  {
+    Serial.println("Sent with success");
+  }
+  else
+  {
+    Serial.println("Error sending the data");
+  }
+  delay(200);
+}
+
+void loop()
+{
+  if (digitalRead(btn0) == LOW)
+  {
+    sendBtn(0);
+  }
+  if (digitalRead(btn1) == LOW)
+  {
+    sendBtn(1);
+  }
+}
